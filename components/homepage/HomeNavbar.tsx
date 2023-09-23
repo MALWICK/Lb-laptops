b4e415c1ff72cca5d4ff78de3ee2579fd05dfcc0
MALWICK <batahumphrey66@gamil.com>
2023-09-23 22:54:24 +0100

    creating naves

---
+++ components/homepage/HomeNavbar.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function HomeNavbar() {
+    const[isSticky, setIsSticky] = useState(false);
+  return (
+    <div>HomeNavbar</div>
+  )
+}
+
+export default HomeNavbar
