cd7784b25d20fb8e94214580ec991d3060d31fff
MALWICK <batahumphrey66@gamil.com>
2023-09-24 20:25:58 +0100

    added new components

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import HomeNavbar from "@/components/homepage/HomeNavbar"
 export default function Home() {
   return (
-    <main className="w-full">
+    <main className="w-full h-[150vh] bg-red-400">
     <HomeNavbar />
     </main>
   )
