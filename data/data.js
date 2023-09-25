bcccf70cf45f1d2e77034c81be8be8778eb55812
MALWICK <batahumphrey66@gamil.com>
2023-09-25 05:14:30 +0100

    updated data from

---
+++ data/data.js
@@ -0,0 +1,12 @@
+export const words = [
+  "Apple MacBook Pro M2",
+  "Dell XPS",
+  "HP Spectre x360",
+  " Lenovo ThinkPad",
+  "Asus ZenBook",
+  "Microsoft Surface Laptop",
+  "Acer Swift",
+  " Razer Blade",
+  "MSI Gaming laptops",
+  " Huawei MateBook",
+];
