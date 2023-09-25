e0cbea4186a17d661ebb17dba112ed317808c6c1
MALWICK <batahumphrey66@gamil.com>
2023-09-25 05:47:27 +0100

    updateing searchbar

--- components/homepage/searchbar.tsx
+++ components/homepage/searchbar.tsx
@@ -5,7 +5,7 @@ import { words } from "@/data/data";
 function searchbar() {
   const [activeSearch, setActiveSearch] = useState<string[]>([]);
 
-  const handleSearch = (e:any) => {
+  const handleSearch = (e: any) => {
     e.preventDefault();
     if (e.target.value === "") {
       setActiveSearch([]);
@@ -23,12 +23,22 @@ function searchbar() {
           className="w-full p-4 rounded-full bg-slate-800"
           id="searchinput"
           placeholder="Type Here"
+          onChange={(e) => handleSearch(e)}
         />
         <button className="absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-slate-300 rounded-full">
           <i className="bi bi-search"></i>
         </button>
       </div>
-      <div className="absolute top-20 p-4 bg-slate-800 text-black w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2"></div>
+
+      {activeSearch.length > 0 && (
+        <div className="absolute top-20 p-4 bg-slate-800 text-black w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2">
+            {
+                activeSearch.map(s => (
+                    <span>{s}</span>
+                ))
+            }
+        </div>
+      )}
     </form>
   );
 }
