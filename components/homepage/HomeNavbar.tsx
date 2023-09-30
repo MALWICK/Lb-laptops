e329944bc69c0a658d70d4bdae45436bc635ca67
MALWICK <batahumphrey66@gamil.com>
2023-09-30 15:35:17 +0100

    updated navbar

--- components/homepage/HomeNavbar.tsx
+++ components/homepage/HomeNavbar.tsx
@@ -1,14 +1,50 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./HomeNavbar.css"
 
 
 function HomeNavbar() {
-  
-  return (
-   <div>
-    
-   </div>
-  );
-}
 
+  const [showMenu, setShowMenu] = useState(false);
+
+  // Function to handle menu toggle
+  const handleMenuToggle = () => {
+    setShowMenu(!showMenu);
+  };
+
+  // Function to handle window resize
+  const handleResize = () => {
+    if (window.innerWidth > 600) {
+      setShowMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="container">
+          <div className="logo">Logo</div>
+          <div className={`menu ${showMenu ? 'show' : ''}`}>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+          </div>
+          <div className="hamburger-menu" onClick={handleMenuToggle}>
+            <div className={`bar ${showMenu ? 'change' : ''}`}></div>
+            <div className={`bar ${showMenu ? 'change' : ''}`}></div>
+            <div className={`bar ${showMenu ? 'change' : ''}`}></div>
+          </div>
+        </div>
+      </nav>
+      <div className="banner"></div>
+    </div>
+  );
+
+  }
 export default HomeNavbar; 
