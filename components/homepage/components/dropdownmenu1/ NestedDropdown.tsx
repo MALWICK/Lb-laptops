e6bfcdc142b058bb9b6454aa5bb3876d63c60d92
MALWICK <batahumphrey66@gamil.com>
2023-10-01 20:13:53 +0100

    updated nested dropdowns

--- components/homepage/components/dropdownmenu1/ NestedDropdown.tsx
+++ components/homepage/components/dropdownmenu1/ NestedDropdown.tsx
@@ -1,69 +1,83 @@
 import React, { useState } from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 
-type MenuItem = Required<MenuProps>['items'][number];
+const { SubMenu } = Menu;
 
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: 'group',
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  } as MenuItem;
-}
-
-const items: MenuItem[] = [
-  getItem('Navigation One', 'sub1', <MailOutlined />, [
-    getItem('Option 1', '1'),
-    getItem('Option 2', '2'),
-    getItem('Option 3', '3'),
-    getItem('Option 4', '4'),
-  ]),
-  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-    getItem('Option 5', '5'),
-    getItem('Option 6', '6'),
-    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-  ]),
-  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Option 11', '11'),
-    getItem('Option 12', '12'),
-  ]),
+const items = [
+  {
+    title: 'Navigation One',
+    key: 'sub1',
+    icon: <MailOutlined />,
+    children: [
+      { title: 'Option 1', key: '1' },
+      { title: 'Option 2', key: '2' },
+      { title: 'Option 3', key: '3' },
+      { title: 'Option 4', key: '4' },
+    ],
+  },
+  {
+    title: 'Navigation Two',
+    key: 'sub2',
+    icon: <AppstoreOutlined />,
+    children: [
+      { title: 'Option 5', key: '5' },
+      { title: 'Option 6', key: '6' },
+      {
+        title: 'Submenu',
+        key: 'sub3',
+        children: [
+          { title: 'Option 7', key: '7' },
+          { title: 'Option 8', key: '8' },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Navigation Three',
+    key: 'sub4',
+    icon: <SettingOutlined />,
+    children: [
+      { title: 'Option 9', key: '9' },
+      { title: 'Option 10', key: '10' },
+      { title: 'Option 11', key: '11' },
+      { title: 'Option 12', key: '12' },
+    ],
+  },
 ];
 
-// submenu keys of first level
 const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
 
-const App: React.FC = () => {
-  const [openKeys, setOpenKeys] = useState(['sub1']);
+const App = () => {
+  const [openKeys, setOpenKeys] = useState<string[]>(['sub1']);
 
-  const onOpenChange: MenuProps['onOpenChange'] = (keys) => {
+  const onOpenChange = (keys: string[]) => {
     const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-    if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
+    if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
       setOpenKeys(keys);
     } else {
       setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
     }
   };
 
+  const renderMenuItems = (menuItems: any[]): React.ReactNode => {
+    return menuItems.map((item) => {
+      if (item.children && item.children.length > 0) {
+        return (
+          <SubMenu key={item.key} icon={item.icon} title={item.title}>
+            {renderMenuItems(item.children)}
+          </SubMenu>
+        );
+      } else {
+        return <Menu.Item key={item.key}>{item.title}</Menu.Item>;
+      }
+    });
+  };
+
   return (
-    <Menu
-      mode="inline"
-      openKeys={openKeys}
-      onOpenChange={onOpenChange}
-      style={{ width: 256 }}
-      items={items}
-    />
+    <Menu mode="inline" openKeys={openKeys} onOpenChange={onOpenChange} style={{ width: 256, background: 'red' }}>
+      {renderMenuItems(items)}
+    </Menu>
   );
 };
 
