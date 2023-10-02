35f852a585ec9ec31831d8a0daadc3d7fba68932
MALWICK <batahumphrey66@gamil.com>
2023-10-02 17:21:43 +0100

    upda

---
+++ data/DropdownOption.ts
@@ -0,0 +1,32 @@
+
+
+export const options = [
+    {
+      title: 'Computers',
+      links: [
+        { title: 'Laptops', link: '/laptops' },
+        { title: 'Desktops', link: '/desktops' },
+        { 
+          title: 'Tablets', 
+          links: [
+            { title: 'Android Tablets', link: '/android-tablets' },
+            { title: 'iPad', link: '/ipad' },
+          ],
+        },
+      ],
+    },
+    {
+      title: 'Computer Accessories',
+      links: [
+        { title: 'Keyboards', link: '/keyboards' },
+        { 
+          title: 'Mice', 
+          links: [
+            { title: 'Wired Mice', link: '/wired-mice' },
+            { title: 'Wireless Mice', link: '/wireless-mice' },
+          ],
+        },
+        { title: 'Monitors', link: '/monitors' },
+      ],
+    },
+  ];
