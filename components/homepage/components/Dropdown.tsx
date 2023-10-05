6e10e4bf1479c7b7f58aa264235ed4a956a66d6f
MALWICK <batahumphrey66@gamil.com>
2023-10-05 12:52:44 +0100

    updating dropdown

--- components/homepage/components/Dropdown.tsx
+++ components/homepage/components/Dropdown.tsx
@@ -40,6 +40,22 @@ const Dropdown = () => {
     { title: "RAMs", url: "/rams" },
   ];
 
+  const AllProductsLinks = [
+    { title: "components& Storage", url: "components&storage"},
+    { title: "Computer System", url: "/computerSystem"},
+    { title: "Computer Peripherals", url: "/computerperipherals"},
+    { title: "Appliances", url: "/appliances" },
+    { title: "Tv & Home Theater ", url: "/tv&homeTheater"},
+    { title: "Gaming & VR", url: "/gaming&vr" },
+    { title: "Smart Home & Security", url: "/smartHome" },
+    { title: "Office Solutions", url: "/officeSolution" },
+    { title: "Software & Services", url: "/softwareServices" },
+    { title: "Automotive & Tools", url: "/automotiveTools" },
+    { title: "Appliances", url: "/homeOutdoors" },
+    { title: "Health & Sports", url: "/healthSports" },
+    { title: "Toys & Drones", url: "/toys&drones" },
+  ];
+
   return (
     <div className="dropdown" ref={dropdownRef}>
       <button className="dropdown-toggle" onClick={toggleDropdown}>
@@ -52,6 +68,16 @@ const Dropdown = () => {
               {pageLinks.map((link) => (
                 <li key={link.title} onClick={() => handleItemClick(link.url)}>
                   {link.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="secondcolumn">
+            <ul className="dropdown-menus">
+              {AllProductsLinks.map((productlink) => (
+                <li key={productlink.title} onClick={() => handleItemClick(productlink.url)}>
+                  {productlink.title}
                 </li>
               ))}
             </ul>
