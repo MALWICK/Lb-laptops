9676219ccf71c2cc5c6aaefe4b30c03df113be54
MALWICK <batahumphrey66@gamil.com>
2023-10-07 00:13:30 +0100

    creating banner data

---
+++ data/BannerData.js
@@ -0,0 +1,12 @@
+export const BannerItem = [
+    { title: "FantasTech || Starts Oct 9", url: "/fantTech" },
+    { title: "Today's Best Deals ", url: "/todaysBestDeals" },
+    { title: "Best Sellers ", url: "/Best Sellers" },
+    { title: "Trade in ", url: "/tradein" },
+    { title: "Clearance ", url: "/clearance" },
+    { title: "Combo Up Saving ", url: "/comboUpsaving" },
+    { title: "Apple Refurb ", url: "/appleRefurb" },
+    { title: "Pc Builder ", url: "/pcBuilder" },
+    { title: "Home Showcase ", url: "/homeShowcase" },
+    { title: "Refurbished Phones ", url: "/refurbishedPhones" },
+]
