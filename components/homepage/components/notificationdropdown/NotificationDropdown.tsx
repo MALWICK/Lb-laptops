6b53a2f22bda9ceb1196eed3d356be1eda03ac5c
MALWICK <batahumphrey66@gamil.com>
2023-10-07 19:48:00 +0100

    updating my notification dropdown

---
+++ components/homepage/components/notificationdropdown/NotificationDropdown.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotificationDropdown() {
+  return (
+    <div className='w-100'>
+        <p>james</p>
+      
+    </div>
+  )
+}
+
+export default NotificationDropdown
