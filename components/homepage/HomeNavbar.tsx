811a2c30c8d2034868f62f33b4fe8171bc410933
MALWICK <batahumphrey66@gamil.com>
2023-10-07 02:16:50 +0100

    updated navbar

--- components/homepage/HomeNavbar.tsx
+++ components/homepage/HomeNavbar.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./HomeNavbar.css";
-import { BsList, BsX } from "react-icons/bs";
+import { BsList, BsX , BsSunFill } from "react-icons/bs";
+import {WiMoonAltWaningCrescent2} from "react-icons/wi";
 import LogoImg from "@/assets/images/lB-logo.png";
 import Image from "next/image";
 import SearchBar from "./components/searchbar";
 import Dropdown from "./components/Dropdown";
-import BannerData from "@/data/BannerData"
+import BannerData from "@/data/BannerData";
 
-function HomeNavbar() {
+interface HomeNavbarProps {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const HomeNavbar: React.FC<HomeNavbarProps> = ({ isDarkMode, toggleDarkMode }) => {
   const [showMenu, setShowMenu] = useState(false);
 
   // Function to handle menu toggle
@@ -23,9 +29,9 @@ function HomeNavbar() {
     }
   };
 
-  const handleLinks = (url:string) => {
+  const handleLinks = (url: string) => {
     window.location.href = url;
-  }
+  };
 
   useEffect(() => {
     window.addEventListener("resize", handleResize);
@@ -33,7 +39,7 @@ function HomeNavbar() {
   }, []);
 
   return (
-    <div>
+    <div className={`bg-${isDarkMode ? 'black' : 'white'} `}>
       <nav className="navbar">
         <div className="container">
           <Dropdown />
@@ -50,6 +56,9 @@ function HomeNavbar() {
 
             <div></div>
           </div>
+          <button className="fixed top-4 right-4 p-2 rounded-full bg-gray-300 dark:bg-gray-800 text-gray-600 dark:text-gray-200" onClick={toggleDarkMode}>
+            {isDarkMode ? <BsSunFill />: <WiMoonAltWaningCrescent2 />}
+          </button>
           <div className="hamburger-menu" onClick={handleMenuToggle}>
             {showMenu ? <BsX /> : <BsList />}
           </div>
@@ -58,7 +67,11 @@ function HomeNavbar() {
       <div className="banner flex justify-center items-center ">
         <ul className="innerbanner flex justify-around items-center w-[80%]  text-base cursor-pointer">
           {BannerData.map((data) => (
-            <li key={data.title} className="hove" onClick={() => handleLinks(data.url)}>
+            <li
+              key={data.title}
+              className="hove"
+              onClick={() => handleLinks(data.url)}
+            >
               {data.title}
             </li>
           ))}
@@ -66,5 +79,5 @@ function HomeNavbar() {
       </div>
     </div>
   );
-}
+};
 export default HomeNavbar;
