2ee294b6708f917979d5a50ba45c08f3452113d4
MALWICK <batahumphrey66@gamil.com>
2023-10-14 14:35:04 +0100

    updating this the searchbar

--- components/homepage/components/searchbar.tsx
+++ components/homepage/components/searchbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { words } from "@/data/data";
+import "./searchbar.css"
 
 function SearchBar() {
   const [activeSearch, setActiveSearch] = useState<string[]>([]);
@@ -88,7 +89,7 @@ function SearchBar() {
   };
 
   return (
-    <form className="w-[500px] relative" onSubmit={handleSubmit}>
+   /*  <form className="w-[500px] relative" onSubmit={handleSubmit}>
       <div className="relative">
         <input
           type="search"
@@ -122,7 +123,43 @@ function SearchBar() {
           ))}
         </div>
       )}
-    </form>
+    </form> */
+
+<form  className="w-[500px] relative" onSubmit={handleSubmit}>   
+    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg className="w-2 h-2 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="searchinput" className="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-orange-500 focus:border-orange-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        placeholder="Search Mockups, Logos..." required 
+        value={searchValue}
+          onChange={handleSearch}
+          onKeyDown={handleKeyDown}
+          onFocus={handleFocus}/>
+        <button type="submit" className="sub text-white absolute right-2.5 bottom-2.5 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    </div>
+    {activeSearch.length > 0 && (
+        <div
+          className="absolute top-20 p-4 bg-slate-100 text-black w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2"
+          ref={suggestionsRef}
+        >
+          {activeSearch.map((suggestion, index) => (
+            <span
+              key={suggestion}
+              onClick={() => handleSuggestionClick(suggestion)}
+              className={selectedSuggestionIndex === index ? "selected" : ""}
+              data-index={index}
+            >
+              {suggestion}
+            </span>
+          ))}
+        </div>
+      )}
+</form>
+
   );
 }
 
