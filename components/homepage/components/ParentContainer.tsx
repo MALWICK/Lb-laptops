2eff496b43d43fd38184fa1591a7990f4830f5e0
MALWICK <batahumphrey66@gamil.com>
2023-10-14 20:39:54 +0100

    updated the parentscontainer

--- components/homepage/components/ParentContainer.tsx
+++ components/homepage/components/ParentContainer.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 function parentcontainer() {
   return (
-    <div>
-      djkdj
+    <div className='w-full bg-red flex items-center justify-center flex-col'>
+      <div className="w-[90%] bg-red flex items-center justify-center flex-col bg-green-600">
+        dopdoppodopo
+      </div>
     </div>
   )
 }
