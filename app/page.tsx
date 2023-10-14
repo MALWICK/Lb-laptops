e98bd0190b439ae5a8d270d7a415e942456fa884
MALWICK <batahumphrey66@gamil.com>
2023-10-14 20:46:42 +0100

    updatingt this parrentcontainer

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import 'primeflex/primeflex.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import ParentContainer from "@/components/homepage/components/ParentContainer";
+import ParentContainer from "@/components/homepage/components/parentcontainer/ParentContainer";
 
 
 const Page: React.FC = () => {
---
+++ components/homepage/components/parentcontainer/ParentContainer.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import "./parentCOntainer.css";
+
+function parentcontainer() {
+  return (
+    <div className="w-full parent bg-red flex items-center justify-center flex-col ">
+      <div className="parentinner bg-red flex items-center justify-center flex-col ">
+        dopdoppodopo
+      </div>
+    </div>
+  );
+}
+
+export default parentcontainer;
