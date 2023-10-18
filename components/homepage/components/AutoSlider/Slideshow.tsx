24d3893e595a9fb0da67920d0566cee17740a34a
MALWICK <batahumphrey66@gamil.com>
2023-10-18 17:11:56 +0100

    updating my images

--- components/homepage/components/AutoSlider/Slideshow.tsx
+++ components/homepage/components/AutoSlider/Slideshow.tsx
@@ -1,7 +1,9 @@
-"use client"
-import { useEffect, useState } from 'react';
-import "./Slideshow.css"
-import RightSidebar from '../RigthSidebar/RightSidebar';
+"use client";
+import { useEffect, useState } from "react";
+import "./Slideshow.css";
+import RightSidebar from "../RigthSidebar/RightSidebar";
+import { FaPlay } from "react-icons/fa";
+import { BsStopCircle } from "react-icons/bs";
 
 interface SlideshowProps {
   images: string[];
@@ -25,7 +27,9 @@ const Slideshow: React.FC<SlideshowProps> = ({ images }) => {
   }, [isPlaying]);
 
   const handlePrev = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
   };
 
   const handleNext = () => {
@@ -35,22 +39,27 @@ const Slideshow: React.FC<SlideshowProps> = ({ images }) => {
   const handleTogglePlay = () => {
     setIsPlaying((prevIsPlaying) => !prevIsPlaying);
   };
-  
+
   const slideStyle = {
     backgroundImage: `url(${images[currentImageIndex]})`,
   };
-  
+
   return (
     <div className="slideshow">
       <div className="slide" style={slideStyle}>
-      <div className="controls">
-        <button className='swiper-button-prev ' onClick={handlePrev}><i className="bi bi-chevron-left"></i></button>
-        
-        <button onClick={handleTogglePlay}>{isPlaying ? 'Stop' : 'Play'}</button>
-        <button className='swiper-button-prev ' onClick={handleNext}><i className="bi bi-chevron-right"></i></button>
+        <div className="controls">
+          <button className="swiper-button-prev " onClick={handlePrev}>
+            <i className="bi bi-chevron-left"></i>
+          </button>
+
+          <button onClick={handleTogglePlay}>
+            {isPlaying ? <BsStopCircle /> : <FaPlay />}
+          </button>
+          <button className="swiper-button-prev " onClick={handleNext}>
+            <i className="bi bi-chevron-right"></i>
+          </button>
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 };
