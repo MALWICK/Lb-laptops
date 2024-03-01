1c0b3930e3a685e0a0042936928eb564a2da04de
MALWICK <batahumphrey66@gmail.com>
2024-03-01 20:05:45 +0100

    updating bottom cards

--- components/homepage/components/TodaysDeals/TodaysDeals.tsx
+++ components/homepage/components/TodaysDeals/TodaysDeals.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import "./TodaysDeal.css"
+import DealsCard from './components/DealsCard'
 
 function TodaysDeals() {
   return (
@@ -8,11 +9,11 @@ function TodaysDeals() {
       <div className="innerDeals__cont w-[100%]">
         <div className="first__row w-full flex items-center justify-between p-2">
             <div className="deals__heading">
-                <h1 className='deals__heading'>TODAY'S BEST DEALS</h1>
+                <h1 className='text-28'>TODAY'S BEST DEALS</h1>
             </div>
             <div className="timer"> crease</div>
         </div>
-
+        <DealsCard />
       </div>
     </div>
   )
