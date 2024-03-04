42aa3b0020f77739b7ec5eaca55aacab9c3927b7
MALWICK <batahumphrey66@gamil.com>
2024-03-04 23:30:59 +0100

    updating codes data

--- components/homepage/components/TodaysDeals/TodaysDeals.tsx
+++ components/homepage/components/TodaysDeals/TodaysDeals.tsx
@@ -5,8 +5,8 @@ import DealsCard from './components/dealsCard/DealsCard'
 
 function TodaysDeals() {
   return (
-    <div className='w-[94%] flex items-center justify-center mt-[4.4%] bg-red-500'>
-      <div className="innerDeals__cont w-[100%]">
+    <div className='w-[94%] flex items-center justify-center mt-[4.4%] h-[50rem]'>
+      <div className="innerDeals__cont w-[100vw]">
         <div className="first__row w-full flex items-center justify-between p-2">
             <div className="deals__heading">
                 <h1 className='text-28'>TODAY'S BEST DEALS</h1>
