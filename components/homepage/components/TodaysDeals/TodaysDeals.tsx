1caaa92e2c2c951535585cb8cfec5f3d5440d0b3
MALWICK <batahumphrey66@gmail.com>
2024-04-14 00:45:36 +0100

    updating this site

--- components/homepage/components/TodaysDeals/TodaysDeals.tsx
+++ components/homepage/components/TodaysDeals/TodaysDeals.tsx
@@ -10,7 +10,7 @@ interface TodaysDealsprop {
 
 const  TodaysDeals: React.FC<TodaysDealsprop> = () => {
   return (
-    <div className='w-[100%] containers   flex items-center justify-center mt-[2.4%] mb-2'>
+    <div className='w-[100%] containers   flex items-center justify-center mt-[2.4%] mb-2 p-3'>
       <div className="innerDeals__cont w-[100vw]">
         <div className="first__row w-full flex items-center justify-between p-2">
             <div className="deals__heading">
