e27b151c9e13c500ce3400ef876c4f413088968d
MALWICK <batahumphrey66@gmail.com>
2024-04-20 16:04:46 +0100

    building weekly drops

--- components/weeklyDrop/productCard.tsx
+++ components/weeklyDrop/productCard.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { FaCirclePlus } from "react-icons/fa6";
+import "./style.css";
 
-const productCard = () => {
+export default function ProductCard(props: {
+  url: string | undefined;
+  name:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | React.PromiseLikeOfReactNode
+    | null
+    | undefined;
+  price:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | React.PromiseLikeOfReactNode
+    | null
+    | undefined;
+  description:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | React.PromiseLikeOfReactNode
+    | null
+    | undefined;
+}) {
   return (
-    <div>productCard</div>
-  )
-}
+    <div className="carde mb-1">
+      <div className="cardeInner_container">
+        <div className="cardeImage-container">
+          <img className="product--image" src={props.url} alt="product image" />
+        </div>
 
-export default productCard
+        <div className="items-name flex items-center justify-between p-2">
+          <h2>{props.name}</h2>
+          <FaCirclePlus />
+        </div>
+        <div className="info-carde p-2 ">
+      
+      
+        <p>gain</p>
+        <p>
+          <span>{props.price}</span>
+        </p>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
--- components/weeklyDrop/weeklyDrop.tsx
+++ components/weeklyDrop/weeklyDrop.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import productCard from './productCard';
+import ProductCard from './productCard';
 import { productData, responsive } from "@/data/Products";
 import { FaCaretRight } from "react-icons/fa6";
 
 function weeklyDrop() {
+    const product = productData.map((item) => (
+        <ProductCard
+          name={item.name}
+          url={item.imageurl}
+          price={item.price}
+          description={item.description}
+        />
+      ));
   return (
-    <div>weeklyDrop</div>
+    <div className="mt-[4rem]	App p-2">
+    <div className="title-hoder w-full p-4 flex items-center  gap-3">
+      <h1 className="text-2xl	 text-blue-950	font-bold">New at Newegg – New Arrivals</h1>
+      <span className='flex items-center justify-normal gap-0 text-sm text-slate-500 hover:underline cursor-pointer'>See More <FaCaretRight/></span>      </div>
+    <Carousel showDots={true} responsive={responsive}>
+      {product}
+    </Carousel>
+  </div>
   )
 }
 
