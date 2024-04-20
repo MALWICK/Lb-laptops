44e60fd3bb064bcd73427b414a182a7080f1a178
MALWICK <batahumphrey66@gmail.com>
2024-04-20 19:41:50 +0100

    updating element

--- components/weeklyDrop/weeklyDrop.tsx
+++ components/weeklyDrop/weeklyDrop.tsx
@@ -17,7 +17,7 @@ function weeklyDrop() {
   return (
     <div className="mt-[4rem]	App p-2">
     <div className="title-hoder w-full p-4 flex items-center  gap-3">
-      <h1 className="text-2xl	 text-blue-950	font-bold">New at Newegg – New Arrivals</h1>
+      <h1 className="text-2xl	 text-blue-950	font-bold">Weekly Deal Drops</h1>
       <span className='flex items-center justify-normal gap-0 text-sm text-slate-500 hover:underline cursor-pointer'>See More <FaCaretRight/></span>      </div>
     <Carousel showDots={true} responsive={responsive}>
       {product}
