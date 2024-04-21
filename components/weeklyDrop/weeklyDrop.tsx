7639b20e28c02ef0f0b668ac25c2391722745661
MALWICK <batahumphrey66@gmail.com>
2024-04-21 01:03:12 +0100

    updated carousel

--- components/weeklyDrop/weeklyDrop.tsx
+++ components/weeklyDrop/weeklyDrop.tsx
@@ -14,12 +14,32 @@ function weeklyDrop() {
           description={item.description}
         />
       ));
+
+      const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 3
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
   return (
     <div className="mt-[4rem] w-[60%]	App p-2">
     <div className="title-hoder w-full p-4 flex items-center  gap-3">
       <h1 className="text-2xl	 text-blue-950	font-bold">Weekly Deal Drops</h1>
       <span className='flex items-center justify-normal gap-0 text-sm text-slate-500 hover:underline cursor-pointer'>See More <FaCaretRight/></span>      </div>
-    <Carousel showDots={true} responsive={responsive}>
+    <Carousel className='gap-[8px]'  responsive={responsive}>
       {product}
     </Carousel>
   </div>
