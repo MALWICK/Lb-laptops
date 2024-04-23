c1f9434ca11cb5fb02d5c33e2f8a0d6f9dedc40b
MALWICK <batahumphrey66@gmail.com>
2024-04-23 18:39:18 +0100

    creating computer azuz baner

---
+++ components/homepage/components/bannerImage/bannerImage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface BannerData {
+    Backgroundimage: string;
+    Title: string;
+}
+
+
+
+const bannerImage = () => {
+  return (
+    <div>bannerImage</div>
+  )
+}
+
+export default bannerImage
