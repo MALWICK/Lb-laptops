b95017ee59b0341c20a18ac93e7f6c6710199c5c
MALWICK <batahumphrey66@gmail.com>
2024-04-23 18:42:31 +0100

    updating banner

--- components/homepage/components/bannerImage/bannerImage.tsx
+++ components/homepage/components/bannerImage/bannerImage.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 interface BannerData {
-    Backgroundimage: string;
-    Title: string;
+    backgroundimage: string;
+    title: string;
 }
 
-
+const Data: BannerData[] = [
+    {
+        backgroundimage: "https://images.unsplash.com/photo-1630911111411-888888888888?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        title: "Title 1"
+    },
+    {
+        backgroundimage: "https://images.unsplash.com/photo-1630911111411-888888888888?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        title: "Title 2"
+    }
+]
 
 const bannerImage = () => {
   return (
