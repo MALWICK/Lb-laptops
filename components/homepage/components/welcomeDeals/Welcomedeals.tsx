16ae4bb2d53980f6769044c59e15df27f8c6a1ec
MALWICK <batahumphrey66@gmail.com>
2024-04-27 20:03:40 +0100

    edited card

--- components/homepage/components/welcomeDeals/Welcomedeals.tsx
+++ components/homepage/components/welcomeDeals/Welcomedeals.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Welcomedeals = () => {
   return (
-    <div className='flex items-center justify-center  w-full   h-[50vh]  bg-slate-500'>
-      <div className="innercontainer w-[80%] flex items-center justify-center h-[30vh] bg-red-700 ">
-     <div className="w-[19vw] bg-orange-200	 flex flex-col p-1 rounded-lg flex-1">
-    <a href="#" className='no-underline	w-full h-full'></a>
-     </div>
+    <div className="flex items-center justify-center  w-full   bg-slate-500">
+      <div className="innercontainer w-[100%] flex items-center justify-center  bg-red-700 ">
+        <div className="w-[300px] bg-red-600 flex flex-col p-1  flex-1">
+          <a
+            href="#"
+            className="no-underline	 h-full cursor-pointer bg-transparent"
+          >
+            <div className="h-[500px] w-[300px] p-4 rounded-lg flex flex-wrap bg-orange-200	"></div>
+          </a>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcomedeals
+export default Welcomedeals;
