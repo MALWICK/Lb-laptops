b4f7f0802f7cae728cf846bfd6ce75cc036f2137
MALWICK <batahumphrey66@gmail.com>
2024-04-27 21:40:03 +0100

    updating this code

--- components/homepage/components/welcomeDeals/Welcomedeals.tsx
+++ components/homepage/components/welcomeDeals/Welcomedeals.tsx
@@ -4,12 +4,17 @@ const Welcomedeals = () => {
   return (
     <div className="flex items-center justify-center  w-full   bg-slate-500">
       <div className="innercontainer w-[100%] flex items-center justify-center  bg-red-700 ">
-        <div className="w-[300px] bg-red-600 flex flex-col p-1  flex-1">
+        <div className="w-[24%] bg-red-600 flex flex-col p-1  flex-1">
           <a
             href="#"
             className="no-underline	 h-full cursor-pointer bg-transparent"
           >
-            <div className="h-[500px] w-[300px] p-4 rounded-lg flex flex-wrap bg-orange-200	"></div>
+            <div className="h-[570px] w-[350px] p-4 rounded-lg flex flex-wrap bg-orange-200	">
+              <div className="text-wrap break-normal flex flex-col">
+              <span className="text-2xl font-extrabold overflow-hidden truncate text-rose-900">Your text goes here</span>
+              <span className=" text-base font-normal">Your exclusive price</span>
+              </div>
+            </div>
           </a>
         </div>
       </div>
