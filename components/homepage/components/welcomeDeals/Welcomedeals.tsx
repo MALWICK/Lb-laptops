29de68ecfd432e9d7d7fcda6af2678687c79a1d5
MALWICK <batahumphrey66@gmail.com>
2024-05-22 13:07:51 +0100

    updating this site

--- components/homepage/components/welcomeDeals/Welcomedeals.tsx
+++ components/homepage/components/welcomeDeals/Welcomedeals.tsx
@@ -1,43 +1,40 @@
 import React from "react";
+import "./style.css"
 
 const Welcomedeals = () => {
   return (
-    <div className="flex items-center justify-center w-full bg-slate-500">
-      <div className="innercontainer w-[100%] flex items-center justify-center bg-red-700 ">
-        <div className="w-[24%] bg-red-600 flex flex-col p-1 flex-1">
-          <div className="no-underline h-full cursor-pointer bg-transparent">
-            <div className="h-[570px] w-[350px] rounded-lg flex flex-wrap bg-orange-200	">
-              <div className="text-wrap break-normal w-full h-[14%] bg-green-500 flex flex-col">
-                <span className="text-2xl font-extrabold overflow-hidden truncate text-rose-900">
-                  Welcome Deal
-                </span>
-                <span className=" text-base font-normal">
-                  Your exclusive price
-                </span>
-              </div>
-              <div className="flex flex-col items-center justify-around w-full h-[80%]">
-                <div className="">
-                  <div className="flex items-center relative flex-col rounded-lg">
-                    <div className="relative overflow-hidden rounded-md">
-                      <img
-                        src="https://c1.neweggimages.com/productimage/nb300/11-854-133-01.jpg"
-                        className=""
-                        alt="Magcubic Projector HY300 PRO 4K Android 11 Dual Wifi6 260ANSI Allwinner H713 BT5.0 1080P 1280*720P Home Cinema Outdoor Projetor"
-                      />
-                    </div>
-                    <div className="flex  max-h[30px] h-full items-center justify-center mt-1">
-                      <div className="innert flex items-center justify-center gap-1">
-                        <span className="flex">￡37.81</span>
-                        <span className="">￡106.58</span>
-                      </div>
-                      <div className="flex absolute"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <div className="flex items-center justify-center w-full ">
+      <div className="innercontainer w-[100%] flex items-center p-4  h-[338px]  rounded-3xl gap-1 bg-orange-500  ">
+        <a
+          href="#"
+          className="w-[441px] pt-5  pr-5 pb-5 pl-5 h-[304px] relative bg-red-400"
+        >
+          <img
+            className="new-user--iconImg--hPKNeKH absolute w-[182px] h-[140px] left-[306px] top-[-11px] object-contain"
+            src="https://upload.shop-bit.com/header/brand_background/1708421400582.png"
+          />
+          <div className="new-user--title--1-OZSf text-white font-medium  max-w-[299px]  w-full text-4xl  leading-tight display-flex overflow-hidden text-ellipsis">
+            New shopper exclusive
+          </div>
+          <div className="new-user--subtitle--lFl2IdN text-2xl max-w-[320px] leading-tight display-flex overflow-hidden text-ellipsis text-white ">
+            First order only
+          </div>
+          <div className="new-user--card--2l9bT00 cardss">
+            <div className="new-user--cardTitle--1vaqt9G small-conts">Up to 70% off</div>
+            <div
+              className="new-user--cardSubtitle--2YvzJT3"
+            
+            >
+              Welcome deal
+            </div>
+            <div
+              className="new-user--cardButton--mX3jj97"
+              
+            >
+              <img src="https://ae01.alicdn.com/kf/S948c53c58f3f456bb087ee24b9602affq/30x23.png" />
             </div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   );
