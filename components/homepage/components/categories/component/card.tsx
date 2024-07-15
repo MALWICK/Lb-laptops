5ca12e21a5fca3ad9b465a417a813d9b056baa3b
MALWICK <batahumphrey66@gmail.com>
2024-07-16 00:50:02 +0100

    updating catergories card

--- components/homepage/components/categories/component/card.tsx
+++ components/homepage/components/categories/component/card.tsx
@@ -1,22 +1,159 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import "./style.css";
+import { Key } from "lucide-react";
+
+interface categoriesData {
+  cardTitle: string;
+  id: number;
+  cardColor: string;
+  catergoryitem: {
+    imageUrl: string;
+    title: string;
+    id: number;
+  }[];
+}
+const Data: categoriesData[] = [
+  {
+    cardTitle: "export bIkes",
+    id: 1,
+    cardColor: "#E0EDFF",
+    catergoryitem: [
+      {
+        imageUrl: "https://promotions.newegg.com/nepro/23-0738/800x120@2x.jpg",
+        title: "Title 1",
+        id: 1,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 2,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 3,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 4,
+      },
+    ],
+  },
+  {
+    cardTitle: "export bIkes",
+    id: 2,
+    cardColor: "#DDEEE0",
+    catergoryitem: [
+      {
+        imageUrl: "https://promotions.newegg.com/nepro/23-0738/800x120@2x.jpg",
+        title: "Title 1",
+        id:1,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 2,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 3,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 4,
+      },
+    ],
+  },
+  {
+    cardTitle: "export bIkes",
+    id: 3,
+    cardColor: "#DCEBF1",
+    catergoryitem: [
+      {
+        imageUrl: "https://promotions.newegg.com/nepro/23-0738/800x120@2x.jpg",
+        title: "Title 1",
+        id: 1,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 2,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 3,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 4,
+      },
+    ],
+  },
+  {
+    cardTitle: "export bIkes",
+    id: 4,
+    cardColor: "#EBE5FF",
+    catergoryitem: [
+      {
+        imageUrl: "https://promotions.newegg.com/nepro/23-0738/800x120@2x.jpg",
+        title: "Title 1",
+        id: 1,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 2,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 3,
+      },
+      {
+        imageUrl: "https://promotions.newegg.com/Group6/24-0418/800x120@2x.jpg",
+        title: "Title 2",
+        id: 4,
+      },
+    ],
+  },
+];
 
 const card = () => {
   return (
-    <div className='grid-col dffg flex-auto w-[400px] h-[450px] border-red-600 border-solid'>
-        <div className="home-tile-title text-lg font-bold mb-5 text-sky-900 leading-7">Outdoor Living</div>
-            <div className="home-tile-box bg-white p-20 pt-0 pr-20 box-border rounded-md flex justify-between flex-wrap">
-                <a href="#" className='home-tile-item'>
-                    <div className="goods-img">
-                        <img src="https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/2f293a63-91ce-4a8b-a5ec-bf0f460cff8e._CR0,142,1613,845_SX860_AGmerlin_QL70_.jpg" alt="" />
-                    </div>
-                    <span className='block text-lg font-bold leading-6 text-gray-700 min-h-16'>Bikes</span>
-                </a>
-            </div>
-       
+    <div className="w-full flex items-center justify-center  gap-2 mt-6">
+      {Data.map((category) => (
+        <div
+          key={category.id}
+          className="grid-col dffg flex-auto w-[400px] h-[450px] border-red-600 border-solid"
+        >
+          <div className="home-tile-title text-lg font-bold mb-5 text-sky-900 leading-7">
+            {category.cardTitle}
+          </div>
+          <div className="home-tile-box bg-white  box-border rounded-md flex flex-wrap justify-center items-center gap-4">
+            {category.catergoryitem.map((item) => (
+              <a href="#" className="home-tile-item" key={item.id}>
+                <div
+                  className="goods-img w-[165px] h-[123px] rounded-xl"
+                  style={{ backgroundColor: category.cardColor }}
+                >
+                  <img src={item.imageUrl} alt={item.title} className="w-[140px] h-[104px] object-cover"/>
+                </div>
+                <span className="block text-lg font-bold leading-6 text-gray-700 min-h-16">
+                  {item.title}
+                </span>
+              </a>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default card
+export default card;
